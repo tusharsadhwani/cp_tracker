3cbf4b2e396ad3cfc5c5d4b453996a29d6c2e20c
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-03-07 17:05:49 +0530

    improved problem screen design

--- lib/screens/problem_screen.dart
+++ lib/screens/problem_screen.dart
@@ -32,68 +32,82 @@ class _ProblemScreenState extends State<ProblemScreen> {
       appBar: AppBar(
         title: Text('View Problem'),
       ),
-      body: FutureBuilder(
-        future: Firestore.instance
-            .collection('tushar')
-            .document(widget.problemId)
-            .get(),
-        builder: (ctx, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            var problem = (snapshot.data as DocumentSnapshot).data;
-            if (problem == null)
-              return Center(
-                child: Text('Deleted Successfully.'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: Firestore.instance
+              .collection('tushar')
+              .document(widget.problemId)
+              .get(),
+          builder: (ctx, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              var problem = (snapshot.data as DocumentSnapshot).data;
+              if (problem == null)
+                return Center(
+                  child: Text('Deleted Successfully.'),
+                );
+              return Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '${problem['platform']} '
+                          '${problem['contest']} '
+                          '${problem['problem']}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        SingleChildScrollView(
+                          child: Container(
+                            width: double.infinity,
+                            child: Text('${problem['notes']}'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    child: RawMaterialButton(
+                      constraints: BoxConstraints(
+                        minHeight: 50,
+                        maxHeight: 50,
+                      ),
+                      elevation: 8.0,
+                      fillColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      onPressed:
+                          _disableButton ? null : () => deleteProblem(ctx),
+                      child: Text(
+                        "Delete this Problem",
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               );
-            return Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                Container(
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        '${problem['platform']} '
-                        '${problem['contest']} '
-                        '${problem['problem']}',
-                      ),
-                      SingleChildScrollView(
-                        child: Text('${problem['notes']}'),
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: 20,
-                  child: RawMaterialButton(
-                    constraints: BoxConstraints(
-                      minHeight: 50,
-                      maxHeight: 50,
-                    ),
-                    elevation: 8.0,
-                    fillColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    onPressed: _disableButton ? null : () => deleteProblem(ctx),
-                    child: Text(
-                      "Delete this Problem",
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          } else
-            return Center(
-              child: Text('Loading...'),
-            );
-        },
+            } else
+              return Center(
+                child: Text('Loading...'),
+              );
+          },
+        ),
       ),
     );
   }
